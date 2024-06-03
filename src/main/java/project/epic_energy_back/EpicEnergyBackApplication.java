dbc8d7110ac745c07304ad428ff347e737e20940
Marcello <marcello.mercanzin@gmail.com>
2024-06-03 15:56:09 +0200

    setup

---
+++ src/main/java/project/epic_energy_back/EpicEnergyBackApplication.java
@@ -0,0 +1,13 @@
+package project.epic_energy_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicEnergyBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpicEnergyBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/epic_energy_back/EpicEnergyBackApplicationTests.java
@@ -0,0 +1,13 @@
+package project.epic_energy_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpicEnergyBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
